b77bcde3d154c8b998f84cd55c25e0b05ad40385
Kevin <kevinhamelink05@gmail.com>
2025-11-24 12:29:54 +0100

    Add automatic currency generator functionality to Fertiliser class

--- Assets/Scripts/idle/generator/Fertiliser.cs
+++ Assets/Scripts/idle/generator/Fertiliser.cs
@@ -10,10 +10,25 @@ public class Fertiliser : UpgradeBase
     
     [SerializeField]private float multiplier;
     [SerializeField]private float baseClick;
+    
+    // Prefab
+    [SerializeField]private GameObject generator;
 
     private void Update()
     {
         addedMultiplier = multiplier;
         addedBaseClick = baseClick;
     }
+    
+    public override void OnBuy(int cost)
+    {
+        if (cost * (amountOwned+1) <= CurrencyManager.instance.currency)
+        {
+            CurrencyManager.instance.TakeCurrency(cost *  (amountOwned+1));
+            OnBuyIncrement();
+
+            Instantiate(generator, transform.position, transform.rotation);
+        }
+    }
+    
 }
---
+++ Assets/Scripts/idle/generator/GeneratorBase.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class GeneratorBase : MonoBehaviour
+{
+    public float baseAmount;
+    public float multiplier;
+
+    public int amountOwned;
+    
+    public void StartGenerating()
+    {
+        InvokeRepeating(nameof(Generate), 1, 1);
+    }
+    
+    public void OnBuyIncrement()
+    {
+        amountOwned += 1;
+    }
+
+    public void OnBuy(int cost)
+    {
+        if (cost * (amountOwned+1) <= CurrencyManager.instance.currency)
+        {
+            CurrencyManager.instance.TakeCurrency(cost *  (amountOwned+1));
+            OnBuyIncrement();
+        }
+    }
+    
+
+    public void Generate()
+    {
+        CurrencyManager.instance.AddCurrency(baseAmount * multiplier);
+    }
+}
