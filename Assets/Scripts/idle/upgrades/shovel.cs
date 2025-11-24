685ec1d37da183fd0f7c87de71ebb2f93b456edb
Jannes <37171@ma-web.nl>
2025-11-24 13:18:09 +0100

    move setting base values from update to start

--- Assets/Scripts/idle/upgrades/shovel.cs
+++ Assets/Scripts/idle/upgrades/shovel.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class shovel : UpgradeBase{
+public class shovel : UpgradeBase
+{
     public override float addedMultiplier{get;set;}
     public override float addedBaseClick{get;set;}
     public override int amountOwned{get;set;}
@@ -11,7 +12,7 @@ public class shovel : UpgradeBase{
     [SerializeField]private float multiplier;
     [SerializeField]private float baseClick;
 
-    private void Update()
+    private void Start()
     {
         addedMultiplier = multiplier;
         addedBaseClick = baseClick;
