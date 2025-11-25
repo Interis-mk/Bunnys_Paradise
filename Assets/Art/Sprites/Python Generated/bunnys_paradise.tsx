54cfb2d36c65b65634ba4da82c361c0a4eee63a6
Kevin <kevinhamelink05@gmail.com>
2025-11-25 09:43:31 +0100

    Remove Python Generated sprites and Map2 scene
    Deleted all assets under Assets/Art/Sprites/Python Generated and removed the Map2.unity scene and its meta file. Also renamed Downloaded Key Set sprite assets to KeyBoardKeys for improved organization.

--- Assets/Art/Sprites/Python Generated/bunnys_paradise.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="bunnys_paradise" tilewidth="20" tileheight="20" tilecount="25" columns="8">
- <image source="tileset_atlas.png" width="160" height="80"/>
-</tileset>
