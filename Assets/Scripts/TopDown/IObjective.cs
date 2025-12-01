551d5154a02404b656a3fe552763af43f853ad4a
julien oving <37832@ma-web.nl>
2025-12-01 14:28:21 +0100

    TEMP

---
+++ Assets/Scripts/TopDown/IObjective.cs
@@ -0,0 +1,7 @@
+﻿
+    public interface IObjective
+    {
+        public bool isActive { get; }
+
+        public bool isCompleted();
+    }
---
+++ Assets/Scripts/TopDown/ObjectiveManager.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ObjectiveManager : MonoBehaviour
+{
+    public List<GameObject> ObjectiveObjects = new List<GameObject>();
+
+    private void Update()
+    {
+        foreach (GameObject objective in ObjectiveObjects)
+        {
+            if (!objective.GetComponent<ObjectiveBase>()) 
+            {
+                
+            }
+        }
+    }
+}
