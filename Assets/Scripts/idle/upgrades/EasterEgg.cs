a918b32e7359a16c1a4088f5c62cc972ddfc3415
Kevin <kevinhamelink05@gmail.com>
2025-12-02 11:03:49 +0100

    Add new upgrade classes and update shopback prefab

--- Assets/Scripts/idle/upgrades/EasterEgg.cs
+++ Assets/Scripts/idle/upgrades/EasterEgg.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandCultivator : UpgradeBase
+public class EasterEgg : UpgradeBase
 {
     public override float addedMultiplier{get;set;}
     public override float addedBaseClick{get;set;}
