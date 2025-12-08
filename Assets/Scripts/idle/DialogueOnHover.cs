0ba23d174fdf18e99842f0368157aba3fb2967f5
Kevin <kevinhamelink05@gmail.com>
2025-12-08 13:09:00 +0100

    Dialogue on hover added, And sprite made

---
+++ Assets/Scripts/idle/DialogueOnHover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class DialogueOnHover : MonoBehaviour
+{
+    public string dialogue;
+    public GameObject prefab;
+    protected GameObject instance;
+    
+    private void OnMouseOver()
+    { 
+        Debug.Log(dialogue);
+       instance = Instantiate(prefab, transform.position, Quaternion.identity);
+       instance.GetComponentInChildren<TextMeshProUGUI>().text = dialogue;
+    }
+    private void OnMouseExit()
+    {
+        Destroy(instance);
+        instance = null;
+    }
+    
+}
