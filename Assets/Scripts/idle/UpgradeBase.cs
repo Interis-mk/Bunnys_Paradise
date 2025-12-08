be2b9d637624c3c777ab1b01c15969fa1c39ab94
julien oving <37832@ma-web.nl>
2025-12-08 14:46:03 +0100

    push

--- Assets/Scripts/idle/UpgradeBase.cs
+++ Assets/Scripts/idle/UpgradeBase.cs
@@ -5,11 +5,15 @@ public abstract class UpgradeBase : MonoBehaviour, IUpgrade
     public virtual float addedMultiplier{get;set;}
     public virtual float addedBaseClick{get;set;}
     public virtual float amountOwned{get;set;}
+    public virtual float currentCost { get; set; }
     public Carrot clickObject{get;set;}
+    public virtual DialogueOnHover OnHover{get;set;}
+    public virtual  string tooltips{get;set;}
 
     public virtual void Start()
     {
         clickObject = FindAnyObjectByType(typeof(Carrot)) as Carrot;
+        OnHover = GetComponent<DialogueOnHover>();
     }
 
     public virtual void OnBuyIncrement()
@@ -17,12 +21,14 @@ public abstract class UpgradeBase : MonoBehaviour, IUpgrade
         clickObject.multiplier += addedMultiplier;
         clickObject.baseAmount += addedBaseClick;
         amountOwned += 1;
+        OnHover.dialogue = $"ClickPower + ${addedBaseClick} " + $"cost : ${currentCost}";
     }
 
     public virtual void OnBuy(int cost)
     {
         if (cost*Mathf.Pow(1.15f, amountOwned) <= CurrencyManager.instance.currency)
         {
+            currentCost = cost * Mathf.Pow(1.15f, amountOwned);
             CurrencyManager.instance.TakeCurrency(cost *  (amountOwned+1));
             OnBuyIncrement();
         }
