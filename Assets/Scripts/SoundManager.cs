abef2fc4434390fbf9d32fbe6cd7f179134bde37
Juliën Oving <ishee.mcyt@gmail.com>
2026-01-13 11:41:08 +0100

    cooking with ishee

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [Serializable]
+    public struct Sfx
+    {
+        public string name;
+        public AudioClip audioFile;
+    }
+    [Serializable]
+    public struct Music
+    {
+        public string name;
+        public AudioClip audioFile;
+    }
+    [Header("sound dictionary's")]
+    public Sfx[] sfx;
+    public Music[] music;
+    public static SoundManager instance;
+    [Header("sound dictionary's")]
+    public AudioSource sfxSource;
+    public AudioSource musicSource;
+    
+    public Dictionary<string, AudioClip> MusicDictionary = new Dictionary<string, AudioClip>();
+    public  Dictionary<string, AudioClip> SfxDictionary = new Dictionary<string, AudioClip>();
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Sfx s in sfx)
+        {
+            SfxDictionary.Add(s.name, s.audioFile);
+        }foreach (Music m in music)
+        {
+            MusicDictionary.Add(m.name, m.audioFile);
+        }
+        
+    }
+
+    public void PlaySfx(string audioName)
+    {
+        sfxSource.PlayOneShot(SfxDictionary[audioName]);
+    }
+    public void SwitchMusic(string audioName)
+    {
+        musicSource.clip = MusicDictionary[audioName];
+        musicSource.Play();
+    }
+}
