088c0670a7d7fade8e761bb3b7d951aa763e50c3
Jannes <37171@ma-web.nl>
2026-01-13 15:00:30 +0100

    dont know what ai just generated, ill find out tomorrow

---
+++ Assets/Scripts/TopDown/Dialogue/DialogueServer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueServer : MonoBehaviour
+{
+    bool active = false;
+    [SerializeField] TextMeshProUGUI dialogueBox;
+    Queue<string> dialogueLines = new Queue<string>();
+    ScriptableObject DialogueContainer;
+    void Start()
+    {
+        dialogueBox.text = "";
+    }
+    
+}
